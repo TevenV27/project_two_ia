c11c688702bea3e4f27cdbf6c8b898baed0e8841
TevenV27 <victoria.kevin@correounivalle.edu.co>
2023-06-04 17:48:50 -0500

    movement of chess pieces

--- app/board.py
+++ app/board.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 
 
-
-
 class Board:
     """
     Representación del tablero de ajedrez.
@@ -11,7 +9,7 @@ class Board:
     TITLE = 'Tablero de ajedrez'
 
     PADDING = 60
-    
+
     SIZE = 8
     SQUARE_SIZE = 60
     BOARD_SIZE = 8 * SQUARE_SIZE + PADDING
@@ -19,7 +17,6 @@ class Board:
     WHITE_COLOR = '#F6FFDE'
     BLACK_COLOR = '#C9DBB2'
     BORDER_COLOR = '#C9DBB2'
-
 
     def __init__(self):
         """
@@ -30,15 +27,24 @@ class Board:
         self.window.title(self.TITLE)
         self.window.resizable(False, False)
 
-        # Incializamos el lienzo.
+        # Inicializamos el lienzo.
         self._init_canvas()
 
-        # Dibujamos y pintamos las casillas de tablero.
+        # Dibujamos y pintamos las casillas del tablero.
         self._paint()
 
         # Colocamos las fichas de ajedrez en el tablero.
         self._place_pieces()
 
+        # Inicializamos las variables para el movimiento de las piezas.
+        self.selected_piece = None
+        self.last_x = None
+        self.last_y = None
+
+        # Configuramos los eventos del ratón.
+        self.canvas.bind("<Button-1>", self._start_drag)
+        self.canvas.bind("<B1-Motion>", self._drag)
+        self.canvas.bind("<ButtonRelease-1>", self._release)
 
     def _init_canvas(self):
         """
@@ -47,7 +53,6 @@ class Board:
         self.canvas = tk.Canvas(self.window, width=self.BOARD_SIZE, height=self.BOARD_SIZE)
         self.canvas.pack()
 
-
     def _paint(self):
         """
         Dibujamos y pintamos las casillas del tablero.
@@ -55,20 +60,32 @@ class Board:
         for row in range(self.SIZE):
             for col in range(self.SIZE):
                 # Calculamos las coordenadas de la esquina superior izquierda de la casilla.
-                x1, y1 = ((col * self.SQUARE_SIZE) + (self.PADDING // 2), (row * self.SQUARE_SIZE) + (self.PADDING // 2))
+                x1, y1 = (
+                    (col * self.SQUARE_SIZE) + (self.PADDING // 2),
+                    (row * self.SQUARE_SIZE) + (self.PADDING // 2),
+                )
 
                 # Calculamos las coordenadas de la esquina inferior derecha de la casilla.
                 x2, y2 = (x1 + self.SQUARE_SIZE, y1 + self.SQUARE_SIZE)
 
                 # Pintamos las casillas de blanco o negro según corresponda.
                 if (row + col) % 2 == 0:
-                    self.canvas.create_rectangle(x1, y1, x2, y2, fill=self.WHITE_COLOR, outline=self.WHITE_COLOR)
+                    self.canvas.create_rectangle(
+                        x1, y1, x2, y2, fill=self.WHITE_COLOR, outline=self.WHITE_COLOR
+                    )
                 else:
-                    self.canvas.create_rectangle(x1, y1, x2, y2, fill=self.BLACK_COLOR, outline=self.WHITE_COLOR)
-        
-        # Dibujamos el borde del tablero.
-        self.canvas.create_rectangle(self.PADDING // 2, self.PADDING // 2, self.BOARD_SIZE - (self.PADDING // 2), self.BOARD_SIZE - (self.PADDING // 2), outline=self.BORDER_COLOR)
+                    self.canvas.create_rectangle(
+                        x1, y1, x2, y2, fill=self.BLACK_COLOR, outline=self.WHITE_COLOR
+                    )
 
+        # Dibujamos el borde del tablero.
+        self.canvas.create_rectangle(
+            self.PADDING // 2,
+            self.PADDING // 2,
+            self.BOARD_SIZE - (self.PADDING // 2),
+            self.BOARD_SIZE - (self.PADDING // 2),
+            outline=self.BORDER_COLOR,
+        )
 
     def _place_pieces(self):
         """
@@ -90,36 +107,168 @@ class Board:
 
         # Colocamos los peones en las casillas correspondientes.
         for col in range(self.SIZE):
-            self.canvas.create_image((col * self.SQUARE_SIZE) + (self.PADDING // 2) + (self.SQUARE_SIZE // 2), self.BOARD_SIZE - (self.PADDING // 2) - (self.SQUARE_SIZE // 2) - self.SQUARE_SIZE, image=self.white_pawn)
-            self.canvas.create_image((col * self.SQUARE_SIZE) + (self.PADDING // 2) + (self.SQUARE_SIZE // 2), (self.PADDING // 2) + (self.SQUARE_SIZE // 2) + self.SQUARE_SIZE, image=self.black_pawn)
+            self.canvas.create_image(
+                (col * self.SQUARE_SIZE)
+                + (self.PADDING // 2)
+                + (self.SQUARE_SIZE // 2),
+                self.BOARD_SIZE - (self.PADDING // 2) - (self.SQUARE_SIZE // 2) - self.SQUARE_SIZE,
+                image=self.white_pawn,
+                tags=("piece",),
+            )
+            self.canvas.create_image(
+                (col * self.SQUARE_SIZE)
+                + (self.PADDING // 2)
+                + (self.SQUARE_SIZE // 2),
+                (self.PADDING // 2) + (self.SQUARE_SIZE // 2) + self.SQUARE_SIZE,
+                image=self.black_pawn,
+                tags=("piece",),
+            )
 
         # Colocamos las torres en las casillas correspondientes.
-        self.canvas.create_image(self.PADDING, self.BOARD_SIZE - self.PADDING, image=self.white_rook)
-        self.canvas.create_image(self.BOARD_SIZE - self.PADDING, self.BOARD_SIZE - self.PADDING, image=self.white_rook)
-        self.canvas.create_image(self.PADDING, self.PADDING, image=self.black_rook)
-        self.canvas.create_image(self.BOARD_SIZE - self.PADDING, self.PADDING, image=self.black_rook)
+        self.canvas.create_image(
+            self.PADDING,
+            self.BOARD_SIZE - self.PADDING,
+            image=self.white_rook,
+            tags=("piece",),
+        )
+        self.canvas.create_image(
+            self.BOARD_SIZE - self.PADDING,
+            self.BOARD_SIZE - self.PADDING,
+            image=self.white_rook,
+            tags=("piece",),
+        )
+        self.canvas.create_image(
+            self.PADDING,
+            self.PADDING,
+            image=self.black_rook,
+            tags=("piece",),
+        )
+        self.canvas.create_image(
+            self.BOARD_SIZE - self.PADDING,
+            self.PADDING,
+            image=self.black_rook,
+            tags=("piece",),
+        )
 
         # Colocamos los alfínes en las casillas correspondientes.
-        self.canvas.create_image(self.PADDING + 2 * self.SQUARE_SIZE, self.BOARD_SIZE - self.PADDING, image=self.white_bishop)
-        self.canvas.create_image(self.BOARD_SIZE - self.PADDING - 2 * self.SQUARE_SIZE, self.BOARD_SIZE - self.PADDING, image=self.white_bishop)
-        self.canvas.create_image(self.PADDING + 2 * self.SQUARE_SIZE, self.PADDING, image=self.black_bishop)
-        self.canvas.create_image(self.BOARD_SIZE - self.PADDING - 2 * self.SQUARE_SIZE, self.PADDING, image=self.black_bishop)
+        self.canvas.create_image(
+            self.PADDING + 2 * self.SQUARE_SIZE,
+            self.BOARD_SIZE - self.PADDING,
+            image=self.white_bishop,
+            tags=("piece",),
+        )
+        self.canvas.create_image(
+            self.BOARD_SIZE - self.PADDING - 2 * self.SQUARE_SIZE,
+            self.BOARD_SIZE - self.PADDING,
+            image=self.white_bishop,
+            tags=("piece",),
+        )
+        self.canvas.create_image(
+            self.PADDING + 2 * self.SQUARE_SIZE,
+            self.PADDING,
+            image=self.black_bishop,
+            tags=("piece",),
+        )
+        self.canvas.create_image(
+            self.BOARD_SIZE - self.PADDING - 2 * self.SQUARE_SIZE,
+            self.PADDING,
+            image=self.black_bishop,
+            tags=("piece",),
+        )
 
         # Colocamos los caballos en las casillas correspondientes.
-        self.canvas.create_image(self.PADDING + self.SQUARE_SIZE, self.BOARD_SIZE - self.PADDING, image=self.white_knight)
-        self.canvas.create_image(self.BOARD_SIZE - self.PADDING - self.SQUARE_SIZE, self.BOARD_SIZE - self.PADDING, image=self.white_knight)
-        self.canvas.create_image(self.PADDING + self.SQUARE_SIZE, self.PADDING, image=self.black_knight)
-        self.canvas.create_image(self.BOARD_SIZE - self.PADDING - self.SQUARE_SIZE, self.PADDING, image=self.black_knight)
+        self.canvas.create_image(
+            self.PADDING + self.SQUARE_SIZE,
+            self.BOARD_SIZE - self.PADDING,
+            image=self.white_knight,
+            tags=("piece",),
+        )
+        self.canvas.create_image(
+            self.BOARD_SIZE - self.PADDING - self.SQUARE_SIZE,
+            self.BOARD_SIZE - self.PADDING,
+            image=self.white_knight,
+            tags=("piece",),
+        )
+        self.canvas.create_image(
+            self.PADDING + self.SQUARE_SIZE,
+            self.PADDING,
+            image=self.black_knight,
+            tags=("piece",),
+        )
+        self.canvas.create_image(
+            self.BOARD_SIZE - self.PADDING - self.SQUARE_SIZE,
+            self.PADDING,
+            image=self.black_knight,
+            tags=("piece",),
+        )
 
         # Colocamos las reinas en las casillas correspondientes.
-        self.canvas.create_image(self.PADDING + 3 * self.SQUARE_SIZE, self.BOARD_SIZE - self.PADDING, image=self.white_queen)
-        self.canvas.create_image(self.PADDING + 3 * self.SQUARE_SIZE, self.PADDING, image=self.black_queen)
+        self.canvas.create_image(
+            self.PADDING + 3 * self.SQUARE_SIZE,
+            self.BOARD_SIZE - self.PADDING,
+            image=self.white_queen,
+            tags=("piece",),
+        )
+        self.canvas.create_image(
+            self.PADDING + 3 * self.SQUARE_SIZE,
+            self.PADDING,
+            image=self.black_queen,
+            tags=("piece",),
+        )
 
         # Colocamos los reyes en las casillas correspondientes.
-        self.canvas.create_image(self.PADDING + 4 * self.SQUARE_SIZE, self.BOARD_SIZE - self.PADDING, image=self.white_king)
-        self.canvas.create_image(self.PADDING + 4 * self.SQUARE_SIZE, self.PADDING, image=self.black_king)
+        self.canvas.create_image(
+            self.PADDING + 4 * self.SQUARE_SIZE,
+            self.BOARD_SIZE - self.PADDING,
+            image=self.white_king,
+            tags=("piece",),
+        )
+        self.canvas.create_image(
+            self.PADDING + 4 * self.SQUARE_SIZE,
+            self.PADDING,
+            image=self.black_king,
+            tags=("piece",),
+        )
 
+    def _start_drag(self, event):
+        """
+        Inicia el arrastre de una pieza cuando se hace clic en ella.
+        """
+        x = self.canvas.canvasx(event.x)
+        y = self.canvas.canvasy(event.y)
+        item = self.canvas.find_closest(x, y)[0]
+        tags = self.canvas.gettags(item)
+        if "piece" in tags:
+            self.selected_piece = item
+            self.last_x = x
+            self.last_y = y
 
+    def _drag(self, event):
+        """
+        Realiza el arrastre de la pieza seleccionada.
+        """
+        x = self.canvas.canvasx(event.x)
+        y = self.canvas.canvasy(event.y)
+        dx = x - self.last_x
+        dy = y - self.last_y
+        self.canvas.move(self.selected_piece, dx, dy)
+        self.last_x = x
+        self.last_y = y
+
+    def _release(self, event):
+        """
+        Libera la pieza y la coloca en la última casilla tocada.
+        """
+        x = self.canvas.canvasx(event.x)
+        y = self.canvas.canvasy(event.y)
+        col = int((x - self.PADDING / 2) // self.SQUARE_SIZE)
+        row = int((y - self.PADDING / 2) // self.SQUARE_SIZE)
+        x_center = (col * self.SQUARE_SIZE) + self.PADDING / 2 + self.SQUARE_SIZE / 2
+        y_center = (row * self.SQUARE_SIZE) + self.PADDING / 2 + self.SQUARE_SIZE / 2
+        self.canvas.coords(self.selected_piece, x_center, y_center)
+        self.selected_piece = None
+        self.last_x = None
+        self.last_y = None
 
 
 if __name__ == '__main__':
